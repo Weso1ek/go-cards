7fbe1e305f7615d02a61f315c88f747a6e360a2f
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-03-03 23:22:01 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+	//var card string = "Ace of Spades"
+
+	//card := "Ace of Spades"
+	//card = "Five of diamonds"
+
+	//card := newCard()
+	//
+	//fmt.Println(card)
+
+	//cards := deck{"Ace of Diamonds", newCard()}
+	//cards = append(cards, "Six of Spades")
+
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+
+	cards := newDeck()
+	//cards.print()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	//fmt.Println(hand)
+	//fmt.Println(remainingCards)
+}
+
+//func newCard() string {
+//	return "Five of Diamonds"
+//}
